22b66142967443d4ad2dc1efff726a97fa5ae93f
Sohaib Khan <sohaibullahkhan@gmail.com>
2022-02-23 16:43:37 +0500

    Update script.js
    Added functionality to check email validation, check length validation, and also password match validation.

--- project 1/script.js
+++ project 1/script.js
@@ -26,20 +26,45 @@ function showSuccess(input) {
 }
 
 // Function to check if email is vaild
-function isvalidEmail(email) {
+function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-} 
+    /* return re.test(String(email).toLowerCase()); */
+    if ( re.test (input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input,`Please provide a valid email`);
+    }
+
+}
 
 // Funcation to check if requried fields have data
 function checkRequried(inputArray) {
     inputArray.forEach(function (input) {
        if (input.value === '') {
+        //console.log(input.id);
            showError(input,`${getFieldId(input)} is requried`);
        } else {
            showSuccess(input);
        }
     });
+}
+
+// Funcation to check length of input field
+function checkLength(input, max, min) {
+    if (input.value.length > min) {
+        showError(input,`${getFieldId(input)} needs to be at least ${min} characters`);
+    } else if (input.value.length < max) {
+        showError(input, `${getFieldId(input)} need to be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    } 
+}
+
+// Funcation to check if password and confirm password match
+function checkPasswordMatch(input1, input2) {
+    if( input1.value !== input2.value ) {
+        showError(input2,"Password don't match")
+    }
 }
 
 // Funcation to get the id of the input field with proper case
@@ -49,7 +74,12 @@ function getFieldId(input) {
 
 // This is an Event Listner for the form on submit
 form.addEventListener('submit', function(e) {
-    // Stop page from reloading on submit
     e.preventDefault(); 
+    
     checkRequried([username,email,password,password2]);
+    checkLength(username,3,10);
+    checkLength(password,6,30);
+    checkEmail(email);
+    checkPasswordMatch(password,password2);
+
 });
